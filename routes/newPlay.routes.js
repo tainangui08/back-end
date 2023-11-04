e2cdfc06531b056fa0aad4df1ca922e91f2099c6
Tainan.Gui <tainanramosgui08@gmail.com>
2023-11-04 20:08:41 -0300

    feat:(newPlay.routes.js):adicionando rotas

--- routes/newPlay.routes.js
+++ routes/newPlay.routes.js
@@ -5,14 +5,19 @@ module.exports = app => {
 
     let router = require("express").Router();
 
+    // rota para criar
     router.post("/", newplay.create);
 
+    //rota pata buscar todos os dados
     router.get("/", newplay.findAll);
 
+    // rota para buscar um dado pelo id
     router.get("/:id", newplay.findOne)
 
+    // rota para atualizar um dado pelo id
     router.put('/:id', newplay.update)
 
+    // rota para deletar um dado pelo id
     router.delete('/:id', newplay.delete)
 
     app.use('/api/newplay', router)
